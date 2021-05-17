5b0c0c35e231736baf2b21ae3b3ea847e5bd25e4
jhonifreitas <freitas.jonathan08@gmail.com>
2021-05-17 14:46:30 -0300

    hub dev

--- functions/src/api/routes/hub-dev.route.ts
+++ functions/src/api/routes/hub-dev.route.ts
@@ -1,11 +1,8 @@
 import { Router } from 'express';
 
 import HubDevController from '../controllers/hub-dev.controller';
-import { ensureAuthentication } from '../middlewares/ensure-authentication';
 
 const hubDevRouter = Router();
-
-hubDevRouter.use(ensureAuthentication);
 
 hubDevRouter.get('/cnpj', HubDevController.getCnpj);
 
