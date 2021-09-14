6addd47077b276241f333d5cc017d6f80c60049e
jhonifreitas <freitas.jonathan08@gmail.com>
2021-09-14 10:10:24 -0300

    route

--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 
 import * as functions from "firebase-functions";
@@ -20,27 +20,21 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use(
-  (
-    err: Error,
-    request: Request,
-    response: Response
-  ) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        code: err.code,
-        message: err.message,
-        status: err.statusCode
-      });
-    }
-
-    console.error(err);
-
-    return response.status(500).json({
-      status: 500,
-      message: 'Internal server error.'
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      code: err.code,
+      message: err.message,
+      status: err.statusCode
     });
   }
-);
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 500,
+    message: 'Internal server error.'
+  });
+});
 
 export const api = regionalFunctions.https.onRequest(app);
--- functions/src/api/routes/hub-dev.route.ts
+++ functions/src/api/routes/hub-dev.route.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 
 import HubDevController from '../controllers/hub-dev.controller';
+import { ensureAuthentication } from '../middlewares/ensure-authentication';
 
 const hubDevRouter = Router();
+
+hubDevRouter.use(ensureAuthentication);
 
 hubDevRouter.get('/cnpj', HubDevController.getCnpj);
 
