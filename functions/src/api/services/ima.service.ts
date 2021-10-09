6973d7ddf519c1a27bc73518ef3b07bcc63158a3
jhonifreitas <freitas.jonathan08@gmail.com>
2021-10-09 11:01:37 -0300

    ima

--- functions/src/api/services/ima.service.ts
+++ functions/src/api/services/ima.service.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
+import { License } from '../../interfaces/ima';
 
 class ImaService {
 
   private url = 'https://consultas.ima.sc.gov.br/consulta/consultar';
 
-  async licenses(cnpj: string): Promise<any> {
+  async licenses(cnpj: string): Promise<License[]> {
     const formData = `consultar=1&cnpj=${cnpj}`
 
     return axios.post(this.url, formData).then(res => {
-      console.log(res.config)
-      return res.data;
+      return Array.isArray(res.data) ? res.data : [];
     });
   }
 }
